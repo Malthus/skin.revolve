c59f13973686e78f4c92d841f4422e6ec5ae3ce0
Malthus <github@marijnhubert.nl>
2015-05-18 21:43:23 +0200

    Improved artwork voor live tv and tv shows in the circle of the Home Menu, fixed loading random items in the Home Menu circle, added numbers to the items in the Home Menu circle, improved Python script for loading Home-properties in window properties.

--- scripts/PopulateStaticItemsFromHomeProperties.py
+++ scripts/PopulateStaticItemsFromHomeProperties.py
@@ -13,8 +13,8 @@ import xbmc
 
 from xbmc_property import *
 
-SCRIPT_NAME = 'Revolve/PopulateListFromHomeProperties'
-DEFAULT_WINDOW = '0'
+SCRIPT_NAME = 'Revolve/PopulateStaticItemsFromHomeProperties'
+DEFAULT_TARGETWINDOW = '0'
 DEFAULT_TARGETMASK = 'MyItems%02dOption'
 TOTAL_ITEMS = 20
 
@@ -22,71 +22,75 @@ def logMessage(annotation):
     message = '%s: %s' % (SCRIPT_NAME, annotation.encode('ascii', 'ignore'))
     xbmc.log(msg=message, level=xbmc.LOGDEBUG)
 
-def createNameProperty(originbase, destination, targetwindow):
-    value = getValueFromHomeProperty(originbase + '.Title')
-    value = replaceEmptyValueFromHomeProperty(originbase + '.EpisodeTitle', value)
-    setValueToProperty(destination, value, targetwindow)
+def createGenericNameProperty(sourcebase, targetproperty, targetwindow):
+    value = getValueFromHomeProperty(sourcebase + '.Title')
+    value = replaceEmptyValueFromHomeProperty(sourcebase + '.EpisodeTitle', value)
+    setValueToProperty(targetproperty, value, targetwindow)
     
-def createSubtitleProperty(originbase, destination, targetwindow):
+def createGenericSubtitleProperty(sourcebase, targetproperty, targetwindow):
     value = joinLabels(
-        getValueFromHomeProperty(originbase + '.ShowTitle'),
-        getValueFromHomeProperty(originbase + '.TVShowTitle'),
-        getValueFromHomeProperty(originbase + '.Studio'),
-        getValueFromHomeProperty(originbase + '.Artist'),
-        getValueFromHomeProperty(originbase + '.Author'),
-        getValueFromHomeProperty(originbase + '.Album'),
-        ignoreNumericZeroValue(getValueFromHomeProperty(originbase + '.Year')),
-        ignoreNumericZeroValue(getValueFromHomeProperty(originbase + '.Version')))
-    setValueToProperty(destination, value, targetwindow)
+        getValueFromHomeProperty(sourcebase + '.ShowTitle'),
+        getValueFromHomeProperty(sourcebase + '.TVShowTitle'),
+        getValueFromHomeProperty(sourcebase + '.Studio'),
+        getValueFromHomeProperty(sourcebase + '.Artist'),
+        getValueFromHomeProperty(sourcebase + '.Author'),
+        getValueFromHomeProperty(sourcebase + '.Album'),
+        ignoreNumericZeroValue(getValueFromHomeProperty(sourcebase + '.Year')),
+        ignoreNumericZeroValue(getValueFromHomeProperty(sourcebase + '.Version')))
+    setValueToProperty(targetproperty, value, targetwindow)
     
-def createIconProperty(originbase, destination, targetwindow):
-    value = getValueFromHomeProperty(originbase + '.Art(poster)')
-    value = replaceEmptyValueFromHomeProperty(originbase + '.Thumb', value)
-    value = replaceEmptyValueFromHomeProperty(originbase + '.Icon', value)
-    setValueToProperty(destination, value, targetwindow)
+def createGenericIconProperty(sourcebase, targetproperty, targetwindow):
+    value = getValueFromHomeProperty(sourcebase + '.Art(poster)')
+    value = replaceEmptyValueFromHomeProperty(sourcebase + '.Thumb', value)
+    value = replaceEmptyValueFromHomeProperty(sourcebase + '.Icon', value)
+    setValueToProperty(targetproperty, value, targetwindow)
     
-def createBackgroundImageProperty(originbase, destination, targetwindow):
-    value = getValueFromHomeProperty(originbase + '.Art(fanart)')
-    value = replaceEmptyValueFromHomeProperty(originbase + '.Art(fanart_image)', value)
-    value = replaceEmptyValueFromHomeProperty(originbase + '.Fanart', value)
-    setValueToProperty(destination, value, targetwindow)
+def createGenericBackgroundImageProperty(sourcebase, targetproperty, targetwindow):
+    value = getValueFromHomeProperty(sourcebase + '.Art(fanart)')
+    value = replaceEmptyValueFromHomeProperty(sourcebase + '.Art(fanart_image)', value)
+    value = replaceEmptyValueFromHomeProperty(sourcebase + '.Fanart', value)
+    setValueToProperty(targetproperty, value, targetwindow)
     
-def createActionProperty(originbase, destination, targetwindow):
-    value = getValueFromHomeProperty(originbase + '.Play')
+def createGenericActionProperty(sourcebase, targetproperty, targetwindow):
+    value = getValueFromHomeProperty(sourcebase + '.Play')
     if value == '':
-        value = addPrefixAndSuffixToLabel(getValueFromHomeProperty(originbase + '.Path'), 'PlayMedia("', '")')
+        value = addPrefixAndSuffixToLabel(getValueFromHomeProperty(sourcebase + '.Path'), 'PlayMedia("', '")')
     if value == '':
-        value = getValueFromHomeProperty(originbase + '.LibraryPath')
-    setValueToProperty(destination, value, targetwindow)
+        value = getValueFromHomeProperty(sourcebase + '.LibraryPath')
+        if "videodb" in value:
+            value = addPrefixAndSuffixToLabel(value, "ActivateWindow(videos,", ")")
+        if "musicdb" in value:
+            value = addPrefixAndSuffixToLabel(value, "ActivateWindow(music,", ")")
+    setValueToProperty(targetproperty, value, targetwindow)
     
 
-def copyProperties(originmask, destinationmask, targetwindow):
+def copyProperties(sourcemask, targetmask, targetwindow):
     for index in range (1, TOTAL_ITEMS):
-        originbase = originmask % (index)
-        destinationbase = destinationmask % (index)
+        sourcebase = sourcemask % (index)
+        targetbase = targetmask % (index)
 
-        createNameProperty(originbase, destinationbase + '.Label', targetwindow)
-        createSubtitleProperty(originbase, destinationbase + '.Label2', targetwindow)
-        createIconProperty(originbase, destinationbase + '.Thumb', targetwindow)
-        createBackgroundImageProperty(originbase, destinationbase + '.Fanart', targetwindow)
-        createActionProperty(originbase, destinationbase + '.Action', targetwindow)
+        createGenericNameProperty(sourcebase, targetbase + '.Label', targetwindow)
+        createGenericSubtitleProperty(sourcebase, targetbase + '.Label2', targetwindow)
+        createGenericIconProperty(sourcebase, targetbase + '.Thumb', targetwindow)
+        createGenericBackgroundImageProperty(sourcebase, targetbase + '.Fanart', targetwindow)
+        createGenericActionProperty(sourcebase, targetbase + '.Action', targetwindow)
 
 
 if len(sys.argv) > 1:
     logMessage('Call to ' + SCRIPT_NAME + ' script with arguments: ' + str(sys.argv) + '.')	
-    originmask = sys.argv[1]
+    sourcemask = sys.argv[1]
 
     if len(sys.argv) > 2:
-        destinationmask = sys.argv[2]
+        targetmask = sys.argv[2]
     else:
-        destinationmask = DEFAULT_TARGETMASK
+        targetmask = DEFAULT_TARGETMASK
     
     if len(sys.argv) > 3:
         targetwindow = sys.argv[3]
     else:
-        targetwindow = DEFAULT_WINDOW
+        targetwindow = DEFAULT_TARGETWINDOW
     
-    logMessage(SCRIPT_NAME + ' copies properties: ' + originmask + ' to ' + destinationmask + ' on window ' + targetwindow)	
-    copyProperties(originmask, destinationmask, targetwindow)
+    logMessage(SCRIPT_NAME + ' copies properties: ' + sourcemask + ' to ' + targetmask + ' on window ' + targetwindow)	
+    copyProperties(sourcemask, targetmask, targetwindow)
 else:
     logMessage(SCRIPT_NAME + ' terminates: Missing argument(s) in call to script.')	
